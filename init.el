83ea28ca079140f24c9802ea513fc9ee54c19531
Felix H. Dahlke <fhd@ubercode.de>
2011-08-29 11:24:36 +0200

    Add nxhtml

--- init.el
+++ init.el
@@ -191,3 +191,6 @@ If no region is defined, all words in the buffer are counted."
 ;;(require 'malabar-mode)
 ;;(setq malabar-groovy-lib-dir (concat malabar-dir "/lib"))
 ;;(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+
+;; nxhtml
+(load "~/.emacs.d/site-lisp/nxhtml/autostart")
