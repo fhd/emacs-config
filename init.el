134f9e955d897343471613329536a7240cfe0878
Felix H. Dahlke <fhd@ubercode.de>
2011-10-29 13:38:29 +0200

    Add vala-mode

--- init.el
+++ init.el
@@ -197,3 +197,10 @@ If no region is defined, all words in the buffer are counted."
 ;; puppet-mode
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
+
+;; vala-mode
+(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
+(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
+(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
+(add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
+(add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
