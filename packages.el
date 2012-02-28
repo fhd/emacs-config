40002d1d45593fdc6ad920e432a488e05f4357fa
Felix H. Dahlke <fhd@ubercode.de>
2012-02-28 13:44:37 +0100

    Add php-mode

--- packages.el
+++ packages.el
@@ -53,4 +53,9 @@
 (require 'mustache-mode)
 (add-to-list 'auto-mode-alist '("\\.mustache$" . mustache-mode))
 
+;; php-mode
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+
 (provide 'packages)
