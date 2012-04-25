94e1aaa8642e5868ce937ab4ec9fb680199b6b3f
Felix H. Dahlke <fhd@ubercode.de>
2012-04-25 19:47:30 +0200

    Make the extensions configurable

--- site-lisp/c-header-guess-mode.el
+++ site-lisp/c-header-guess-mode.el
@@ -1,14 +1,23 @@
+(defvar c-header-guess-c++-extensions '("cpp" "cc" "cxx"))
+(defvar c-header-guess-objc-extensions '("m" "mm"))
+
+(defun c-header-guess-files-exist (basename extensions)
+  (loop for extension in extensions
+        if (file-exists-p (concat basename "." extension))
+        return t))
+
+(defun c-header-guess-mode-for-basename (basename)
+  (cond
+   ((c-header-guess-files-exist basename c-header-guess-c++-extensions)
+    'c++-mode)
+   ((c-header-guess-files-exist basename c-header-guess-objc-extensions)
+    'obcj-mode)
+   ('c-mode)))
+
 ;;;###autoload
 (defun c-header-guess-mode ()
   (interactive)
-  (let* ((basename (replace-regexp-in-string "\\.h$" "" (buffer-file-name)))
-         (cpp-exists (or (file-exists-p (concat basename ".cpp"))
-                         (file-exists-p (concat basename ".cc"))))
-         (m-exists (or (file-exists-p (concat basename ".m"))
-                       (file-exists-p (concat basename ".mm")))))
-    (cond
-     (cpp-exists (c++-mode))
-     (m-exists (objc-mode))
-     ((c-mode)))))
+  (funcall (c-header-guess-mode-for-basename
+            (replace-regexp-in-string "\\.h$" "" (buffer-file-name)))))
 
 (provide 'c-header-guess-mode)
