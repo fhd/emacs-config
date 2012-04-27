e5edf93df0082a99b8f93b09f1fc1cb0b8409165
Felix H. Dahlke <fhd@ubercode.de>
2012-04-27 11:10:38 +0200

    Use ruby-mode for Gemfiles

--- packages.el
+++ packages.el
@@ -46,7 +46,8 @@
 (add-to-list 'file-coding-system-alist '("\\.vala$" . utf-8))
 (add-to-list 'file-coding-system-alist '("\\.vapi$" . utf-8))
 
-;; ruby-mode for Vagrantfile
+;; ruby-mode for Gemfile and Vagrantfile
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 ;; mustache-mode
