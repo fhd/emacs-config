dac986fa7f1a4eef1e48892fb1295352999c0522
Felix H. Dahlke <fhd@ubercode.de>
2013-04-09 06:47:00 +0200

    Set the terminal colors correctly in Emacs 24.3

--- global.el
+++ global.el
@@ -14,12 +14,25 @@
 (setq ido-enable-flex-matching t)
 (setq ido-auto-merge-work-directories-length -1)
 (ido-mode t)
+
 (if (>= emacs-major-version 24)
     (load-theme 'wombat t))
 (setq custom-enabled-themes '(wombat))
-(setq ansi-term-color-vector [unspecified
-                              "grey30" "red2" "spring green" "yellow2"
-                              "steel blue" "magenta2" "cyan2" "white"])
+(if (and (= emacs-major-version 24) (< emacs-minor-version 3))
+    (setq ansi-term-color-vector [unspecified
+                                  "grey30" "red2" "spring green" "yellow2"
+                                  "steel blue" "magenta2" "cyan2" "white"])
+  (custom-set-faces
+   `(term-color-black ((t (:foreground "grey30" :background "grey30"))))
+   `(term-color-red ((t (:foreground "red2" :background "red2"))))
+   `(term-color-green ((t (:foreground "spring green" :background "spring green"))))
+   `(term-color-yellow ((t (:foreground "yellow2" :background "yellow2"))))
+   `(term-color-blue ((t (:foreground "steel blue" :background "steel blue"))))
+   `(term-color-magenta ((t (:foreground "magenta2" :background "magenta2"))))
+   `(term-color-cyan ((t (:foreground "cyan2" :background "cyan2"))))
+   `(term-color-white ((t (:foreground "white" :background "white"))))
+   '(term-default-fg-color ((t (:inherit term-color-white))))
+   '(term-default-bg-color ((t (:inherit term-color-black))))))
 
 (when window-system
   (tool-bar-mode -1)
