766ef221915984efcc4500e88c8345833b07a7ee
Felix H. Dahlke <fhd@uberco.de>
2024-04-17 11:12:39 +0200

    Set fill-column to 80

--- site-lisp/basics.el
+++ site-lisp/basics.el
@@ -1,5 +1,10 @@
+;; Save backups in /tmp
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+;; Set fill-column to 80 (except in org-mode)
+(setq-default fill-column 80)
+(add-hook 'org-mode-hook (lambda () (setq-local fill-column 70)))
 
 (use-package server
   :config
