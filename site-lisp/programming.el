4a8fb57dbde0c337e84b1dedb41210e7de123496
Felix H. Dahlke <fhd@uberco.de>
2024-04-17 19:22:59 +0200

    Add TODO

--- site-lisp/programming.el
+++ site-lisp/programming.el
@@ -5,6 +5,7 @@
   ;; TODO: Neither TAB nor M-TAB work, using corfu-auto as a fallback.
   (setq corfu-auto 1))
 
+;; TODO: Disable angle brackets as pairs in js-mode, web-mode, possibly others.
 (use-package smartparens
   :ensure t
   :hook (prog-mode . smartparens-strict-mode)
