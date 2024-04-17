4df9899ac16be7140381c3e1732c700673da6e3a
Felix H. Dahlke <fhd@uberco.de>
2024-04-17 11:06:20 +0200

    Add smartparens

--- site-lisp/programming.el
+++ site-lisp/programming.el
@@ -1,11 +1,16 @@
-;; TODO: Add paredit or smartparens.
-
 (use-package corfu
   :ensure t
   :config
   (global-corfu-mode)
   ;; TODO: Neither TAB nor M-TAB work, using corfu-auto as a fallback.
   (setq corfu-auto 1))
+
+(use-package smartparens
+  :ensure t
+  :hook (prog-mode . smartparens-mode)
+  :bind (:map smartparens-mode-map ("M-(" . sp-wrap-round))
+  :config
+  (require 'smartparens-config))
 
 (use-package treemacs
   :ensure t
