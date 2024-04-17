67e2e0e9d3b077682aa14ff0a8d7322453d5deb2
Felix H. Dahlke <fhd@uberco.de>
2024-04-17 11:17:18 +0200

    Show the fill column indicator

--- site-lisp/visual.el
+++ site-lisp/visual.el
@@ -2,4 +2,7 @@
   :ensure t
   :config (load-theme 'dracula t))
 
+(add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'text-mode-hook 'display-fill-column-indicator-mode)
+
 (provide 'visual)
