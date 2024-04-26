95c6035d754dc47ffefbd1b1dec257d7ae240f45
Felix H. Dahlke <fhd@uberco.de>
2024-04-26 22:31:58 +0200

    Use smartparens in slime-mode

--- site-lisp/my-programming.el
+++ site-lisp/my-programming.el
@@ -13,8 +13,7 @@
   :ensure t
   :hook ((lisp-data-mode . smartparens-strict-mode)
          (clojure-mode . smartparens-strict-mode)
-         ;; TODO: Only slime, not run-python etc.
-         ;;(comint-mode . smartparens-strict-mode)
+         (slime-mode . smartparens-strict-mode)
          (cider-repl-mode . smartparens-strict-mode))
   :bind (:map smartparens-mode-map
               ("M-(" . sp-wrap-round)
