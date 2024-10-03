fdbe2048bd41a1490dde67d7961b360f63afe174
Felix H. Dahlke <fhd@uberco.de>
2024-10-03 09:00:41 +0200

    Support plantuml in org-mode

--- site-lisp/my-org.el
+++ site-lisp/my-org.el
@@ -2,4 +2,8 @@
 (setq org-export-allow-bind-keywords t)
 (setq org-clock-display-default-range 'untilnow)
 
+;; Support plantuml in org-mode
+(org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t)))
+(setq org-plantuml-jar-path (expand-file-name plantuml-jar-path))
+
 (provide 'my-org)
