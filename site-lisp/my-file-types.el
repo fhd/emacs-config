fe9f86fefb4486428276e5ba359b20e501c616a8
Felix H. Dahlke <fhd@uberco.de>
2024-10-09 14:26:45 +0200

    Use web-mode for .razor files

--- site-lisp/my-file-types.el
+++ site-lisp/my-file-types.el
@@ -24,7 +24,7 @@
 (use-package web-mode
   :ensure t
   :defer t
-  :mode ("\\.html$" "\\.tsx$")
+  :mode ("\\.html$" "\\.tsx$" "\\.razor$")
   :config
   (setq web-mode-enable-auto-indentation nil))
 
