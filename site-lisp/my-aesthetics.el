d9443bc437af93f15e73af986813c1394b56fb2d
Felix H. Dahlke <fhd@uberco.de>
2024-11-05 13:19:29 +0100

    Set font to Hack, 10 dp

--- site-lisp/my-aesthetics.el
+++ site-lisp/my-aesthetics.el
@@ -5,4 +5,8 @@
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'text-mode-hook 'display-fill-column-indicator-mode)
 
+(let ((font "Hack 10"))
+  (if (find-font (font-spec :name font))
+      (add-to-list 'default-frame-alist (cons 'font font))))
+
 (provide 'my-aesthetics)
