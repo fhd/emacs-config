fbb22edbcbaf0ceecf8725b80fd97b0de07ed26c
Felix H. Dahlke <fhd@uberco.de>
2024-11-07 08:13:44 +0100

    Add TODO

--- site-lisp/my-aesthetics.el
+++ site-lisp/my-aesthetics.el
@@ -5,6 +5,9 @@
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'text-mode-hook 'display-fill-column-indicator-mode)
 
+;; TODO: On one machine, this doesn't work when I launch Emacs with "Emacs
+;; (Client)". It does work when I launch it with "Emacs (Gui)". Find a
+;; workaround.
 (let ((font "Hack 10"))
   (when (find-font (font-spec :name font))
     (set-frame-font font)
