81df5666e3a175b5ec4929cf8e4448145b0a29f7
Felix H. Dahlke <fhd@uberco.de>
2024-11-13 08:41:24 +0100

    Remove font setting workaround

--- site-lisp/my-aesthetics.el
+++ site-lisp/my-aesthetics.el
@@ -5,12 +5,4 @@
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'text-mode-hook 'display-fill-column-indicator-mode)
 
-;; TODO: On one machine, this doesn't work when I launch Emacs with "Emacs
-;; (Client)". It does work when I launch it with "Emacs (Gui)". Find a
-;; workaround.
-(let ((font "Hack 10"))
-  (when (find-font (font-spec :name font))
-    (set-frame-font font)
-    (add-to-list 'default-frame-alist (cons 'font font))))
-
 (provide 'my-aesthetics)
