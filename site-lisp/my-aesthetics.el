ed3bc02353d97f048b9ab710c00a5ebec064de09
Felix H. Dahlke <fhd@uberco.de>
2024-11-22 16:53:21 +0100

    Add visual-fill-column package

--- site-lisp/my-aesthetics.el
+++ site-lisp/my-aesthetics.el
@@ -5,4 +5,9 @@
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'text-mode-hook 'display-fill-column-indicator-mode)
 
+;; TODO: Looks a wee bit ugly with all those soft wrap markers and no margin.
+(use-package visual-fill-column
+  :ensure t
+  :config (global-visual-fill-column-mode))
+
 (provide 'my-aesthetics)
