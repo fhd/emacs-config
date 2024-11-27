ada662dfaa78bd8fd293dd97ca5d35c38b07cbf0
Felix H. Dahlke <fhd@uberco.de>
2024-11-27 15:43:59 +0100

    Improve the look of the fill column indicator

--- site-lisp/my-aesthetics.el
+++ site-lisp/my-aesthetics.el
@@ -1,6 +1,10 @@
 (use-package dracula-theme
   :ensure t
-  :config (load-theme 'dracula t))
+  :config
+  (load-theme 'dracula t)
+  (let ((color "#2f3855"))
+    (set-face-attribute 'fill-column-indicator
+                        nil :height 1 :foreground color :background color)))
 
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'text-mode-hook 'display-fill-column-indicator-mode)
