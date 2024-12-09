03a94c0f20a55743f38a1de32f0c5969d122cd77
Felix H. Dahlke <fhd@uberco.de>
2024-12-09 19:22:53 +0100

    Add verb

--- site-lisp/my-org.el
+++ site-lisp/my-org.el
@@ -1,11 +1,20 @@
-(setq org-duration-format 'h:mm)
-(setq org-export-allow-bind-keywords t)
-(setq org-clock-display-default-range 'untilnow)
+(use-package org
+  :ensure nil
+  :config
+  (setq org-duration-format 'h:mm)
+  (setq org-export-allow-bind-keywords t)
+  (setq org-clock-display-default-range 'untilnow)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)
-   (shell . t)
-   (lisp . t)))
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)
+     (shell . t)
+     (lisp . t))))
+
+(use-package verb
+  :ensure t
+  :after org
+  :config
+  (define-key org-mode-map (kbd "C-c C-r") verb-command-map))
 
 (provide 'my-org)
