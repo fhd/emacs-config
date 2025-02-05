1c5f2e1d3fa384586bb045c1231b3a128fc1b8fd
Felix H. Dahlke <fhd@uberco.de>
2025-02-05 10:49:42 +0100

    Add a function to send a quick prompt with a region as context

--- site-lisp/my-utilities.el
+++ site-lisp/my-utilities.el
@@ -4,7 +4,8 @@
          ("C-c g s" . gptel-send)
          ("C-c g r" . gptel-rewrite)
          ("C-c g a" . gptel-add)
-         ("C-c g m" . gptel-menu))
+         ("C-c g m" . gptel-menu)
+         ("C-c g p" . my-gptel-prompt))
   :config
   (setq gptel-default-mode 'org-mode)
   (if-let* ((auth-info
@@ -15,7 +16,32 @@
   (if-let* ((auth-info
              (auth-source-search :host "generativelanguage.googleapis.com" :user "apikey"))
             (secret (plist-get (car auth-info) :secret)))
-      (gptel-make-gemini "Gemini" :stream t :key secret)))
+      (gptel-make-gemini "Gemini" :stream t :key secret))
+
+  (defun my-gptel-prompt ()
+    (interactive)
+    (let ((prompt (read-string "Enter prompt: "))
+          (text (when (region-active-p)
+                  (buffer-substring-no-properties (region-beginning) (region-end)))))
+      (unless (string-empty-p prompt)
+        (message "Querying %s..." (gptel-backend-name gptel-backend))
+        (gptel-request (if text
+                           (concat prompt "\n\n" text)
+                         prompt)
+          :callback
+          (lambda (response info)
+            (when response
+              (let ((buffer-name (format "*%s Response*" (gptel-backend-name gptel-backend))))
+                (with-temp-buffer-window
+                    buffer-name
+                    '(display-buffer-in-side-window
+                      (side . bottom)
+                      (slot . 0))
+                    (lambda (window _value)
+                      (with-selected-window window
+                        (local-set-key "q" #'kill-buffer-and-window)))
+                  (princ response))
+                (select-window (get-buffer-window buffer-name))))))))))
 
 (use-package google-this
   :ensure t
